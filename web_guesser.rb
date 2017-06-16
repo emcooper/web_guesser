1ab9b6e6c1bcbeecd57eda8c90a201761b828dbd
Ellen Cooper <ellencooper74@gmail.com>
2017-06-16 10:49:29 -0700

    adds background color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,16 +6,18 @@ R_NUMBER = rand(1..100)
 
 get '/' do
   guess = params["guess"]
-  feedback = check_guess(guess)
-  erb :index, :locals => {:feedback => feedback}
+  feedback = check_guess(guess)[0]
+  color = check_guess(guess)[1]
+  erb :index, :locals => {:feedback => feedback,
+                          :color => color}
 end
 
 def check_guess(guess)
   dif = guess.to_i - R_NUMBER
-  return "Way too high!" if dif > 5
-  return "Too high!" if dif < 5 && dif > 0
-  return "Too low!" if dif > -5 && dif < 0
-  return "Way too low!" if dif < -5
-  return "Correct! The secret number is #{R_NUMBER}" if dif == 0
-  return "" if params["guess"].nil?
+  return ["Way too high!", "#8B0000"] if dif > 5
+  return ["Too high!", "#CD5C5C"] if dif < 5 && dif > 0
+  return ["Too low!", "#CD5C5C"] if dif > -5 && dif < 0
+  return ["Way too low!", "#8B0000"] if dif < -5
+  return ["Correct! The secret number is #{R_NUMBER}", "#98FB98"] if dif == 0
+  return [""] if params["guess"].nil?
 end
