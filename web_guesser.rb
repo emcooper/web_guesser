953a2c4233c5d914aeb256bd83382540e08f328f
Ellen Cooper <ellencooper74@gmail.com>
2017-06-15 22:35:27 -0700

    completes game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-r_number = rand(1..100)
+R_NUMBER = rand(1..100)
+
 
 get '/' do
-  erb :index, :locals => {:number => r_number}
+  guess = params["guess"]
+  feedback = check_guess(guess)
+  erb :index, :locals => {:feedback => feedback}
+end
+
+def check_guess(guess)
+  dif = guess.to_i - R_NUMBER
+  return "Way too high!" if dif > 5
+  return "Too high!" if dif < 5 && dif > 0
+  return "Too low!" if dif > -5 && dif < 0
+  return "Way too low!" if dif < -5
+  return "Correct! The secret number is #{R_NUMBER}" if dif == 0
+  return "" if params["guess"].nil?
 end
